32699829f79fa375295137918888e7772e1e7077
krishnabala <krishnabs334@gamil.com>
2024-11-17 15:33:49 +0530

    corrected the backend

--- src/index.js
+++ src/index.js
@@ -18,13 +18,26 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(express.json());
 
+// Allowed origins
+const allowedOrigins = ["https://incandescent-beijinho-0e58b0.netlify.app", "http://localhost:5174"];
+
+// CORS options
 const corsOptions = {
-  origin: ["http://localhost:5174", "http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: "Content-Type,Authorization",
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
+  methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 //Define Routes
 app.use("/api/v1/users", userRouter);
