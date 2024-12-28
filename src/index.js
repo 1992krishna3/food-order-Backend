bf23d8cacd5aea68b6acc6218b3928d860c901c9
krishnabala <krishnabs334@gamil.com>
2024-12-28 13:52:40 +0530

    modified backend

--- src/index.js
+++ src/index.js
@@ -34,8 +34,23 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  withCredentials: true,
+  credentials: true,
 })); 
+
+// Global Middleware to set headers for CORS responses
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+  next();
+});
 
 //Define Routes
 app.use("/api/v1/users", userRouter);
