56fc9b19a8d78fada32e4a6421155bcccd5e9cd7
krishnabala <krishnabs334@gamil.com>
2025-01-02 23:31:51 +0530

    modified backend

--- src/index.js
+++ src/index.js
@@ -20,37 +20,19 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(express.json());
 
-// CORS configuration
-const allowedOrigins = [
-  "https://capstone-foodorderdelivery-project.netlify.app",
-  "http://localhost:5173",
-];
+
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: [
+    "https://capstone-foodorderdelivery-project.netlify.app",
+    "http://localhost:5173",
+  ],
+  credentials:true,
+    methods:['GET',"POST","PUT","DELETE","OPTIONS","PATCH"]
+  
 })); 
 
-// Global Middleware to set headers for CORS responses
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.status(204).end();
-  }
-  next();
-});
+
 
 //Define Routes
 app.use("/api/v1/users", userRouter);
