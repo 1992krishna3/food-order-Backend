310f9af03502e1871c4dfc5d450288b56ce5a7c8
krishnabala <krishnabs334@gamil.com>
2025-01-04 09:17:30 +0530

    backend changed

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ app.use(cors({
   origin: [
     "https://capstone-foodorderdelivery-project.netlify.app",
     "http://localhost:5173",
+    
   ],
   credentials:true,
     methods:['GET',"POST","PUT","DELETE","OPTIONS","PATCH"]
